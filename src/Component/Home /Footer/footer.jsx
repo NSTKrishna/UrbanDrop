45a1c6843376b0539524093fc8307af074fc4c0c
NSTKrishna <krishnagehlot936@gmail.com>
2025-05-03 01:22:19 +0530

    footer update

--- src/Component/Home /Footer/footer.jsx
+++ src/Component/Home /Footer/footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <div>
+      <div>
+        <div className="flex justify-center items-center flex-col h-[300px] bg-blue-800 text-white border-b">
+          <h1 className="font-extrabold text-[32px] py-1">
+            Ready to Get Started?
+          </h1>
+          <p>
+            Book your shipment today and experience our reliable logistics
+            services
+          </p>
+          <div className="flex gap-5 py-5">
+            <button className="hover:text-blue-600 hover:bg-white p-1 rounded">
+              Sign Up Free
+            </button>
+            <button className="hover:text-blue-600 hover:bg-white p-1 rounded">
+              Login In
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="bg-blue-800 text-white py-10 px-5 flex flex-col items-center">
+        <p>© 2025 InterCity Logistics. All rights reserved.</p>
+      </div>
+    </div>
   )
-}
+};
 
-export default footer
+export default footer;
--- src/Component/Home /Navbar/nav.jsx
+++ src/Component/Home /Navbar/nav.jsx
@@ -5,37 +5,37 @@ const nav = () => {
   return (
     <div className="flex items-center justify-between px-6 py-4 backdrop-blur shadow sticky top-0 z-50">
       <div className="font-extrabold text-lg hover:blue-500">
-        <h1 className="hover:text-blue-500">UrbanDrop</h1>
+        <Link to="/" className="hover:text-blue-500">UrbanDrop</Link>
       </div>
 
       <div className="flex font-extrabold items-center gap-6">
         <nav className="flex gap-6">
           <Link
-            className=" px-2 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all "
+            className=" px-2 text-blue-500 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all "
             to="/"
           >
             Home
           </Link>
           <Link
-            className="px-2 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
+            className="px-2 text-blue-500 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
             to="/services"
           >
             Services
           </Link>
           <Link
-            className="px-2 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
+            className="px-2 text-blue-500 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
             to="/tracking"
           >
             Tracking
           </Link>
           <Link
-            className="px-2 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
+            className="px-2 text-blue-500 py-1 hover:bg-blue-500 hover:text-white hover:rounded transform hover:scale-105 transition-all"
             to="/contact"
           >
             Contact
           </Link>
         </nav>
-        <button className="font-semibold  bg-blue-400 text-[#FFEEC6] hover:bg-[#FFEEC6] hover:text-blue-400 px-2 py-2 rounded">
+        <button className="font-semibold  bg-blue-400 text-[#FFEEC6] hover:bg-[#FFEEC6] hover:text-blue-400 px-2 py-2 rounded transform hover:scale-110 transition-all">
           Book Now
         </button>
       </div>

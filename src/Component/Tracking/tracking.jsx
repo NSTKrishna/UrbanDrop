1c7b1e9b07ec80eed634fa3c549fc3650c9a44b4
NSTKrishna <krishnagehlot936@gmail.com>
2025-05-05 16:49:26 +0530

    static tracking is update

--- src/Component/Tracking/tracking.jsx
+++ src/Component/Tracking/tracking.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-
+import Footer from "../Home /Footer/Footer";
 const tracking = () => {
   return (
     <div>
-      <div className="flex justify-center items-center h-[300px] bg-blue-500 text-white flex-col">
-        <div className="text-center">
-          <h1>Track Your Shipment</h1>
-          <p>
+      <div className="flex justify-center items-center h-[500px] bg-blue-500 text-white flex-col">
+        <div className="text-center py-10">
+          <h1 className="font-extrabold text-[48px]">Track Your Shipment</h1>
+          <p className="text-[20px]">
             Enter your tracking Id to get real-time updates on your shipment
           </p>
         </div>
         <div className="flex-start shadow-lg bg-white text-black p-5 rounded-lg w-[700px]">
-          <h1>Shipment Tracking</h1>
-          <p>Enter your tracking number below</p>
-          <div className="flex flex-row gap-2">
-            <input className='w-full' type="text" placeholder="Enter your tracking number" />
+          <h1 className="font-bold text-[27px] pt-5">Shipment Tracking</h1>
+          <p className="text-[16px] text-zinc-500">Enter your tracking number below</p>
+          <div className="flex flex-row py-5 gap-2">
+            <input className='w-full border rounded p-2' type="text" placeholder="Enter tracking number (e.g., MH757888565)" />
             <button>Track</button>
           </div>
         </div>
       </div>
+      <Footer/>
     </div>
   );
 };
